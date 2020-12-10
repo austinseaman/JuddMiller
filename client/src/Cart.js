c18b44477e409207a6f3df2adb607ffbc8961e2a
austinseaman <apseaman0@gmail.com>
2020-12-10 14:37:06 -0700

    done testing a thing

--- client/src/Cart.js
+++ client/src/Cart.js
@@ -4,7 +4,6 @@ export default function Cart() {
     return (
         <div className='cart-main'>
             <h1>Your Cart</h1>
-            <h2>Here's the cart where stuff ppl wanna buy will go and stuff.</h2>
             <button className="btn">Checkout</button>
         </div>
     )
